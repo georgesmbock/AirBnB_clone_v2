5e13775e234a36a4f204d404a25c6b81c65f13fa
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-07-12 01:58:45 +0100

    all

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+fafric script that generates a /tgz from the contents of web_static
+"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """
+    return the archive path if archive has bbeenn correctly
+    """
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%m%S")
+    archived_path = "versions/web_stactic_{}.tgz".format(date)
+    t_gzip_archive == local("tar -cvzf {} web_static".format(archived_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_path
+    else:
+        return None
